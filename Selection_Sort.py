068d12e11e73acc2dc2246701a300368d3933cd8
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-10-26 17:17:48 -0600

    t

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,7 +1,7 @@
 # nums = [10, 70, 30, 100, 40, 45, 90, 80, 85]
 # words = ["dog","at", "good", "eye", "cat", "ball", "fish"]
 
-def SelectionSort(list):
+def SelectionSort_My_Way(list):
         n=0
         for n in range(n,len(list)-1):
             minimum = min(list[n:len(list)])
@@ -10,17 +10,10 @@ def SelectionSort(list):
                     (list[n], list[pos]) = (list[pos], list[n]) 
                     n+=1
             n+=1
-# SelectionSort(nums)
-# print(nums)
-# SelectionSort(words)
-# print(words)
-
-
-
 
 
 #proper way
-def selectionSort(anArray):
+def SelectionSort_Normal_Way(anArray):
         arraysize = len(anArray) 
         for ind in range(arraysize): 
                 minIndex = ind
@@ -29,6 +22,14 @@ def selectionSort(anArray):
                                 minIndex = j 
                 (anArray[ind], anArray[minIndex]) = (anArray[minIndex], anArray[ind]) 
 
+
+
+
+
+# SelectionSort(nums)
+# print(nums)
+# SelectionSort(words)
+# print(words)
 
 # selectionSort(nums) 
 # selectionSort(words) 
